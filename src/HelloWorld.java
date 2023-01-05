d7023d20138d34ca1933c6f8b1e6d9e305773eb7
Matheus Cassiano <matheuscassiano6467@gmail.com>
2023-01-05 13:01:56 -0300

    Configurações

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    public static int soma(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        int resultado = HelloWorld.soma(2, 5);
+        System.out.println(resultado);
+    }
+}
