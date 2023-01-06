d261deb76bc833919168413fece36c4f43bab831
Matheus Cassiano <matheuscassiano6467@gmail.com>
2023-01-06 15:42:49 -0300

    Tratando melhor a jogada Promoção

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,7 +40,11 @@ public class Program {
 				}
 				if (chessMatch.getPromoted() != null) {
 					System.out.print("Entre com uma peça para promoção (B/N/R/Q): ");
-					String type = sc.nextLine();
+					String type = sc.nextLine().toUpperCase();
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						System.out.print("Valor inválido! Entre com uma peça para promoção (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
 					chessMatch.replacePromotedPiece(type);
 				}
 			} catch (ChessException e) {
